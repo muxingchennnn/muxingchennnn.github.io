cfc1d471ab1988c63c4f5b196ea96f8914d0d9da
Muxing Chen <ike.chan@outlook.com>
2024-01-01 14:08:05 -0500

    project info update

--- src/lib/utilities/projects.js
+++ src/lib/utilities/projects.js
@@ -1,10 +1,10 @@
 const data = [
   {
     id: 1,
-    title: 'Hurricane Landfalls Explorer',
+    title: 'US Hurricane Landfalls Explorer',
     types: ['Interactive Visualization'],
-    skills: ['D3.js', 'HTML/CSS/JS'],
-    source: 'https://hurricane-landfalls.vercel.app/',
+    skills: ['SvelteKit', 'D3.js'],
+    source: 'https://us-hurricane-landfalls.vercel.app/',
   },
   {
     id: 2,
