f53c4de55c90de1e06dc163bda4db460afaed9db
Muxing Chen <ike.chan@outlook.com>
2024-01-01 23:04:36 -0500

    npm install flowbite and shadcn-svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,9 @@
 <script>
-	import '../app.css';
-	import { dev } from '$app/environment';
-	import { inject } from '@vercel/analytics';
+  import '../app.pcss'
+  import { dev } from '$app/environment'
+  import { inject } from '@vercel/analytics'
 
-	inject({ mode: dev ? 'development' : 'production' });
+  inject({ mode: dev ? 'development' : 'production' })
 </script>
 
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,37 +1,48 @@
 <script>
-	import Notice from '$lib/components/Notice.svelte';
-	import Header from '$lib/components/Header.svelte';
-	import Intro from '$lib/components/Intro.svelte';
-	import Work from '$lib/components/Work.svelte';
-	import project from '$lib/utilities/projects.js';
-	import { useLenis } from '$lib/utilities/lenis.js'
+  import Notice from '$lib/components/Notice.svelte'
+  import Header from '$lib/components/Header.svelte'
+  import Intro from '$lib/components/Intro.svelte'
+  import Work from '$lib/components/Work.svelte'
+  import project from '$lib/utilities/projects.js'
+  import { useLenis } from '$lib/utilities/lenis.js'
+  import { Toast } from 'flowbite-svelte'
+  import { fly } from 'svelte/transition'
 
-	let hoveredProject = null;
+  let hoveredProject = null
 
-	function handleHover(id, isHovered) {
-		hoveredProject = isHovered ? id : null;
-	}
+  function handleHover(id, isHovered) {
+    hoveredProject = isHovered ? id : null
+  }
 </script>
 
-<main use:useLenis>
-	<section>
-		<Notice />
-		<!-- <Header /> -->
-		<Intro />
-	</section>
-	<p class="font-romie italic text-2xl">Work</p>
-	{#each project as prop (prop.id)}
-		<Work
-			{prop}
-			on:hover={(e) => handleHover(prop.id, e.detail)}
-			hovered={hoveredProject === prop.id}
-			notHovered={hoveredProject !== null && hoveredProject !== prop.id}
-		/>
-	{/each}
+<main use:useLenis class="relative">
+  <section>
+    <Notice />
+    <!-- <Header /> -->
+    <Intro />
+  </section>
+  <p class="font-romie italic text-2xl">Work</p>
+  {#each project as prop (prop.id)}
+    <Work
+      {prop}
+      on:hover={(e) => handleHover(prop.id, e.detail)}
+      hovered={hoveredProject === prop.id}
+      notHovered={hoveredProject !== null && hoveredProject !== prop.id}
+    />
+  {/each}
+  <Toast
+    transition={fly}
+    params={{ x: 200 }}
+    position="top-right"
+    divClass="bg-gray-700"
+  >
+    Welcome!👋 &nbsp I'm gradually building up my site and refining my works.🤯 <br
+    /><br />Stay tuned for something awesome!✨
+  </Toast>
 </main>
 
 <style>
-	section {
-		/* height: 100vh; */
-	}
+  section {
+    /* height: 100vh; */
+  }
 </style>
