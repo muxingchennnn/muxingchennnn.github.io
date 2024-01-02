1c4383cb8b48b6320bfd0d95eb4bf8ef35c7f625
Muxing Chen <ike.chan@outlook.com>
2024-01-02 12:12:28 -0500

    config prettier and eslint

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,16 +3,10 @@
   import Header from '$lib/components/Header.svelte'
   import Intro from '$lib/components/Intro.svelte'
   import Work from '$lib/components/Work.svelte'
-  import project from '$lib/utilities/projects.js'
+
   import { useLenis } from '$lib/utilities/lenis.js'
   import { Toast } from 'flowbite-svelte'
   import { fly } from 'svelte/transition'
-
-  let hoveredProject = null
-
-  function handleHover(id, isHovered) {
-    hoveredProject = isHovered ? id : null
-  }
 </script>
 
 <main use:useLenis>
@@ -21,15 +15,7 @@
     <!-- <Header /> -->
     <Intro />
   </section>
-  <p class="font-romie italic text-2xl">Work</p>
-  {#each project as prop (prop.id)}
-    <Work
-      {prop}
-      on:hover={(e) => handleHover(prop.id, e.detail)}
-      hovered={hoveredProject === prop.id}
-      notHovered={hoveredProject !== null && hoveredProject !== prop.id}
-    />
-  {/each}
+  <Work />
 </main>
 
 <Toast
