478fe91aaddd7c5a0e3e9b94855d532afc9390ce
Muxing Chen <ike.chan@outlook.com>
2025-03-07 14:57:07 -0500

    initial commit with fonts loaded and basic styles set up

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<script>
+	import '../app.css';
+	import '@fontsource-variable/source-serif-4';
+	import '@fontsource-variable/source-serif-4/opsz.css';
+	import '@fontsource-variable/source-serif-4/wght-italic.css';
+	import '@fontsource-variable/source-serif-4/opsz-italic.css';
+	import '@fontsource-variable/geist';
+
+	let { children } = $props();
+</script>
+
+{@render children()}
+
+<style>
+	@font-face {
+		font-family: 'Romie';
+		font-style: italic;
+		font-display: swap;
+		font-weight: 400;
+		src: url($lib/fonts/Romie-Italic.woff2) format('woff2');
+	}
+</style>
