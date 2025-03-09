c834ea6d96c8e4be795a80eb118a949c83707cb0
Muxing Chen <ike.chan@outlook.com>
2025-03-08 20:07:41 -0500

    add Intro component

---
+++ src/lib/components/Intro.svelte
@@ -0,0 +1,36 @@
+<section class="grid-ctn">
+	<p class="col-span-full col-start-1 font-[200] text-(--color-gray-500)">
+		Hi, I'm <span class="text-(--color-foreground)">Muxing Chen</span>
+	</p>
+	<!-- <div class="hr"></div> -->
+	<p class="header col-span-full col-start-1 w-full text-[4rem] font-bold text-nowrap">
+		Information Visualization
+	</p>
+	<!-- <div class="bg-foreground h-[1px] w-full"></div> -->
+	<div
+		class="col-span-full col-start-1 flex w-fit flex-col gap-[clamp(0.125rem,calc(-0.1667rem_+_1.2963vw),1rem)]"
+	>
+		<p class="header font-display text-wrap">Researcher, Designer & Developer</p>
+		<!-- <div class="hr"></div> -->
+		<p class="last-line self-end font-[200] text-(--color-gray-500)">
+			among many other <span class="font-display hightlight text-(--color-foreground)"
+				>pursuits</span
+			>
+		</p>
+	</div>
+</section>
+
+<style>
+	.header {
+		font-size: clamp(1.25rem, 0.3333rem + 4.0741vw, 4rem);
+		line-height: 1em;
+	}
+
+	.hightlight {
+		font-size: clamp(1.125rem, 0.8333rem + 1.2963vw, 2rem);
+	}
+
+	/* .last-line {
+		margin-top: clamp(0.25rem, -0.0588rem + 1.1765vw, 1rem);
+	} */
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import Grid from "$lib/components/Grid.svelte";
+	import Intro from "$lib/components/Intro.svelte";
+</script>
+
+<div class="h-16"></div>
+<Grid />
+<Intro />
+
+<!-- <p>testing if shaking</p>
+<p>testing if shaking</p> -->
+
+<style>
+</style>
