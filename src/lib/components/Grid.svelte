a02dbd0b35056a13f528d51734720042a3a6f5d4
Muxing Chen <ike.chan@outlook.com>
2025-03-08 20:07:23 -0500

    add Grid component

---
+++ src/lib/components/Grid.svelte
@@ -0,0 +1,37 @@
+<script>
+	let columns = $state(0);
+	let width = $state(400);
+
+	function getBreakpointValue(size) {
+		// Get the computed value of the CSS custom property
+		let styles = getComputedStyle(document.documentElement);
+		let breakpoint = styles.getPropertyValue(`--breakpoint-${size}`);
+
+		return parseInt(breakpoint.replace("px", ""));
+	}
+
+	$inspect(getBreakpointValue("xs"));
+
+	function updateColumns(width) {
+		if (width >= getBreakpointValue("lg")) {
+			columns = 12;
+		} else if (width >= getBreakpointValue("sm")) {
+			columns = 8;
+		} else {
+			columns = 4;
+		}
+	}
+
+	$effect(() => {
+		updateColumns(width);
+	});
+</script>
+
+<svelte:window bind:innerWidth={width} />
+<div class="grid-ctn">
+	{#each Array(columns) as _, index}
+		<div class="col-span-1 bg-gray-500 text-center text-base font-bold">
+			Col{index + 1}
+		</div>
+	{/each}
+</div>
