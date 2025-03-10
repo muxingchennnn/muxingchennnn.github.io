a0698c950321ee360478cc8a03823c73d5b7fd2a
Muxing Chen <ike.chan@outlook.com>
2025-03-10 00:06:29 -0400

    load font from static folder

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,9 @@
 	import "../app.css";
 	import "lenis/dist/lenis.css";
 	import "@fontsource-variable/source-serif-4";
-	import "@fontsource-variable/source-serif-4/opsz.css";
+	// import "@fontsource-variable/source-serif-4/opsz.css";
 	import "@fontsource-variable/source-serif-4/wght-italic.css";
-	import "@fontsource-variable/source-serif-4/opsz-italic.css";
+	// import "@fontsource-variable/source-serif-4/opsz-italic.css";
 	import "@fontsource-variable/geist";
 	import { windowWidth } from "$lib/globalStates.svelte";
 
@@ -12,10 +12,16 @@
 </script>
 
 <svelte:window bind:innerWidth={windowWidth.value} />
+<nav>
+	<ul class="flex gap-4">
+		<li><a href="/">Home</a></li>
+		<li><a href="/resume">Resume</a></li>
+	</ul>
+</nav>
 {@render children()}
 
 <style>
-	@font-face {
+	/* @font-face {
 		font-family: "Romie";
 		font-style: italic;
 		font-display: swap;
@@ -23,5 +29,15 @@
 		src:
 			url($lib/fonts/Romie-Italic.woff2) format("woff2"),
 			url($lib/fonts/Romie-Italic.woff) format("woff");
+	} */
+
+	@font-face {
+		font-family: "Romie";
+		font-style: italic;
+		font-display: swap;
+		font-weight: 400;
+		src:
+			url(/fonts/Romie-Italic.woff2) format("woff2"),
+			url(/fonts/Romie-Italic.woff) format("woff");
 	}
 </style>
