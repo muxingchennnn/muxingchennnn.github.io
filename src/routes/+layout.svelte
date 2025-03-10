69465de5697c9ed1bf7ec32aaf80e63142d73af0
Muxing Chen <ike.chan@outlook.com>
2025-03-10 00:29:26 -0400

    use fonts.css to load font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,7 @@
 <script>
 	import "../app.css";
+	import "$lib/styles/fonts.css";
 	import "lenis/dist/lenis.css";
-	import "@fontsource-variable/source-serif-4";
-	// import "@fontsource-variable/source-serif-4/opsz.css";
-	import "@fontsource-variable/source-serif-4/wght-italic.css";
-	// import "@fontsource-variable/source-serif-4/opsz-italic.css";
-	import "@fontsource-variable/geist";
 	import { windowWidth } from "$lib/globalStates.svelte";
 
 	let { children } = $props();
@@ -21,23 +17,4 @@
 {@render children()}
 
 <style>
-	/* @font-face {
-		font-family: "Romie";
-		font-style: italic;
-		font-display: swap;
-		font-weight: 400;
-		src:
-			url($lib/fonts/Romie-Italic.woff2) format("woff2"),
-			url($lib/fonts/Romie-Italic.woff) format("woff");
-	} */
-
-	@font-face {
-		font-family: "Romie";
-		font-style: italic;
-		font-display: swap;
-		font-weight: 400;
-		src:
-			url(/fonts/Romie-Italic.woff2) format("woff2"),
-			url(/fonts/Romie-Italic.woff) format("woff");
-	}
 </style>
