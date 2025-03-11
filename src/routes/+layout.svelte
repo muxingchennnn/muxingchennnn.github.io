8dc841c5a0f75dbbb52fa1e4fd403be28379252d
Muxing Chen <ike.chan@outlook.com>
2025-03-11 16:02:47 -0400

    bind innerHeight to global windowHeight

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,13 +2,13 @@
 	import "../app.css";
 	import "$lib/styles/fonts.css";
 	import "lenis/dist/lenis.css";
-	import { windowWidth } from "$lib/globalStates.svelte";
+	import { windowWidth, windowHeight } from "$lib/globalStates.svelte";
 	import { lenis, smoothScroll } from "$lib/utils/lenis";
 
 	let { children } = $props();
 </script>
 
-<svelte:window bind:innerWidth={windowWidth.value} />
+<svelte:window bind:innerWidth={windowWidth.value} bind:innerHeight={windowHeight.value} />
 <nav>
 	<ul class="flex gap-4">
 		<li><a href="/">Home</a></li>
