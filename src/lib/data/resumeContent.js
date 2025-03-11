d9e1d3d58c52437353629684de213a5c28eb43f7
Muxing Chen <ike.chan@outlook.com>
2025-03-10 23:00:43 -0400

    add resume content json

---
+++ src/lib/data/resumeContent.js
@@ -0,0 +1,156 @@
+export const resumeContent = [
+	{
+		section: "EXPERIENCE",
+		items: [
+			{
+				company: "MIT Senseable City Lab",
+				title: "Data Visualization Specialist",
+				date: "Jul 2024 - Aug 2024",
+				location: "Cambridge, MA",
+				description: [
+					"Collaborated with researchers to refine storylines and interaction techniques in website mockups.",
+					"Designed and developed a scrollytelling website for an urban study paper in the lab."
+				],
+				url: "https://senseable.mit.edu/"
+			},
+			{
+				company: "Dubberly Design Office",
+				title: "Information Designer & Researcher",
+				date: "Jul 2023 - Dec 2023",
+				location: "San Francisco, CA",
+				description: [
+					"Created a concept map of Figma to provide users with a clear overview of the application.",
+					"Developed conceptual models for Figma, Sketchpad and early versions of Illustrator to foster understanding of 2D drawing applications.",
+					"Conducted extensive research and attempted to develop a more robust theory for design systems."
+				],
+				url: "https://www.dubberly.com/"
+			}
+		]
+	},
+	{
+		section: "RESEARCH",
+		items: [
+			{
+				institution: "Northeastern University",
+				title: "Research Assistant, NSF Awards Analysis Project",
+				supervisor: "Enrico Bertini",
+				date: "Jul 2023 - Dec 2023",
+				location: "Boston, MA",
+				description: [
+					"Performed topic modeling and preliminary visual analytics on the NSF awards dataset using Bertopic and Tableau.",
+					"Prototyped a streamlined interface that allows users to search for awards efficiently and delightfully."
+				]
+			},
+			{
+				institution: "Northeastern University",
+				title: "Research Assistant, ECA-PRO Project",
+				supervisor: "Enrico Bertini",
+				date: "Aug 2022 - Dec 2023",
+				location: "Boston, MA",
+				description: [
+					"Designed visualizations for patient self-reported data and two interface prototypes for patients and clinicians respectively.",
+					"Proposed a rendering pipeline to automate data narratives that walk patients through their health data."
+				]
+			},
+			{
+				institution: "Northeastern University",
+				title:
+					"MFA Thesis: The Whole Is Other Than The Sum Of Its Parts: A framework for classifying and deconstructing composite visualization",
+				supervisor: "Paolo Ciuccarelli",
+				date: "Sept 2022 - Apr 2023",
+				location: "Boston, MA",
+				description: [
+					"Formulated a framework that identifies ten composition patterns for combining multiple visualizations to form a cohesive whole (composite visualization) in the same visual space, based on spatial and data relations between component visualizations."
+				]
+			}
+		]
+	},
+	{
+		section: "EDUCATION",
+		items: [
+			{
+				institution: "Northeastern University",
+				degree: "PhD in Interdisciplinary Design and Media",
+				date: "Sept 2024 -",
+				location: "Boston, MA",
+				description: []
+			},
+			{
+				institution: "Northeastern University",
+				degree: "MFA in Information Design and Data Visualization",
+				date: "Sept 2021 - May 2024",
+				location: "Boston, MA",
+				description: []
+			},
+			{
+				institution: "CUNY, Baruch College",
+				degree: "BBA in Accounting, Double Minor in Mathematics and Data Analytics",
+				date: "Jun 2019 - May 2021",
+				location: "New York, NY",
+				description: []
+			},
+			{
+				institution: "Southwestern University of Finance and Economics",
+				degree: "BBA in Accounting",
+				date: "Sept 2015 - Jun 2018",
+				location: "Chengdu, China",
+				description: []
+			}
+		]
+	},
+	{
+		section: "SKILLS",
+		items: [
+			{
+				skillType: "Programming",
+				skills: [
+					"Javascript",
+					"D3.js",
+					"p5.js",
+					"GSAP (GreenSock)",
+					"Svelte",
+					"SvelteKit",
+					"React",
+					"HTML",
+					"CSS",
+					"Tailwind CSS",
+					"Supabase",
+					"Python",
+					"R"
+				]
+			},
+			{
+				skillType: "Data Visualization Tools",
+				skills: ["Tableau", "Observable", "Flourish", "Datawrapper", "RAWGraphs"]
+			},
+			{
+				skillType: "Design",
+				skills: ["Figma", "Adobe Illustrator", "Adobe InDesign", "Adobe After Effects", "Webflow"]
+			}
+		]
+	},
+	{
+		section: "ECs",
+		items: [
+			{
+				organization: "AIESEC in Swufe",
+				title: "President, Talent Management Department",
+				date: "Nov 2016 - Nov 2017",
+				location: "Chengdu, China",
+				description: [
+					"Developed and implemented an evaluation model to analyze 71 members’ performance and growth.",
+					"Led a 10-member committee to orchestrate a forum attended by 200+ participants from multiple universities across Chengdu."
+				]
+			},
+			{
+				organization: "Excel Education Foundation",
+				title: "Volunteer",
+				date: "Jul 2017 - Aug 2017",
+				location: "Kilimanjaro, Tanzania",
+				description: [
+					"Prepared promotional materials that facilitated connections between local children and worldwide education sponsors."
+				]
+			}
+		]
+	}
+];
