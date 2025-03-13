4a3352ccd7874253f39a1642644a0aa85c271d05
Muxing Chen <ike.chan@outlook.com>
2025-03-13 15:31:28 -0400

    refine view transition animations in layout for improved performance and add new animation for indicator group

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -21,13 +21,13 @@
 <style>
 	/* These properties seem bypass Svelte's default CSS scoping */
 	::view-transition-old(root) {
-		animation: 300ms cubic-bezier(0, 0, 0.2, 1) both fade-out;
+		animation: 300ms both cubic-bezier(0, 0, 0.2, 1) fade-out;
 		/* animation: 1.5s cubic-bezier(0.87, 0, 0.13, 1) both move-out; */
 	}
 
 	::view-transition-new(root) {
 		/* duration / delay / timing-function / fill-mode / keyframes */
-		animation: 1s 800ms cubic-bezier(0, 0, 0.2, 1) both fade-in;
+		animation: 500ms 800ms ease-in both fade-in;
 		/* animation: 1.5s cubic-bezier(0.87, 0, 0.13, 1) both move-in; */
 	}
 
@@ -41,6 +41,13 @@
 	/* ::view-transition-old(identity),
 	::view-transition-new(identity) {
 	} */
+
+	::view-transition-group(indicator) {
+		animation-duration: 300ms;
+		/* "small-bounce": cubic-bezier(.99, 1.6, 0, .72) from https://gsap.com/community/forums/topic/38174-from-css-cubic-bezier-to-gsap-ease/*/
+		animation-timing-function: cubic-bezier(0.99, 1.6, 0, 0.72);
+		animation-fill-mode: both;
+	}
 
 	@keyframes fade-in {
 		from {
