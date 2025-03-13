b2d0d4df20ced7211485ef395b3d1d8167b7c299
Muxing Chen <ike.chan@outlook.com>
2025-03-13 00:05:26 -0400

    add Nav component for navigation bar with links to Home and Resume

---
+++ src/lib/components/Nav.svelte
@@ -0,0 +1,23 @@
+<script>
+	import { page } from "$app/state";
+
+	const navBarTabs = [
+		{ path: "/", label: "Home" },
+		{ path: "/resume", label: "Resume" }
+	];
+</script>
+
+<nav class="flex gap-4">
+	{#each navBarTabs as { path, label }}
+		<a
+			href={path}
+			onclick={(e) => {
+				if (page.url.pathname === path) {
+					e.preventDefault();
+				}
+			}}
+		>
+			{label}
+		</a>
+	{/each}
+</nav>
