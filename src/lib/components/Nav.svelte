c6c76f1250c63c4fbf320d9d7c53a0b03a9e025c
Muxing Chen <ike.chan@outlook.com>
2025-03-13 12:42:43 -0400

    refactor Nav component to update tab labels, enhance styling with view transitions, and improve layout structure

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -2,22 +2,52 @@
 	import { page } from "$app/state";
 
 	const navBarTabs = [
-		{ path: "/", label: "Home" },
+		{ path: "/", label: "Work" },
+		// { path: "/", label: "Research" },
 		{ path: "/resume", label: "Resume" }
+		// { path: "/gallery", label: "Gallery" }
 	];
 </script>
 
-<nav class="flex gap-4">
-	{#each navBarTabs as { path, label }}
-		<a
-			href={path}
-			onclick={(e) => {
-				if (page.url.pathname === path) {
-					e.preventDefault();
-				}
-			}}
-		>
-			{label}
-		</a>
-	{/each}
+<nav
+	class="card-style sticky top-0 z-50 mx-auto flex w-full justify-between px-6 py-4"
+	style:view-transition-name="navBar"
+>
+	<div>Logo</div>
+	<div class="flex gap-8">
+		{#each navBarTabs as { path, label }}
+			<a
+				class="relative"
+				class:page-indicator={page.url.pathname === path}
+				href={path}
+				onclick={(e) => {
+					if (page.url.pathname === path) {
+						e.preventDefault();
+					}
+				}}
+			>
+				{label}
+			</a>
+		{/each}
+	</div>
 </nav>
+
+<style>
+	@reference "../../app.css";
+
+	.page-indicator {
+		@apply after:absolute after:bottom-[-0.5em] after:left-1/2 after:h-[0.4em] after:w-[0.4em] after:translate-x-[-50%] after:rounded-full after:bg-gray-700;
+	}
+
+	.page-indicator::after {
+		view-transition-name: indicator;
+	}
+
+	/* prettier-ignore */
+	.card-style {
+		@apply 
+    rounded-b border-b border-solid 
+  bg-gray-100/60 border-gray-200 
+    backdrop-blur-xs;
+	}
+</style>
